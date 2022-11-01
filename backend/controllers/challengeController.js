500ecc5ebf6c69b086fbd658ff91d5f47b90a369
Beth-Anne Farmer <bethannef@protonmail.com>
2022-10-31 23:47:33 -0400

    complete crud functionality

--- backend/controllers/challengeController.js
+++ backend/controllers/challengeController.js
@@ -1,34 +1,67 @@
+const asyncHandler = require('express-async-handler');
+
+const Challenge = require('../models/challengeModel');
 // @desc get challenge
 // @route GET /api/challenge
 // @access Private
 
-const getChallenge = (req, res) => {
-  res.status(200).json({message: 'get challenge'});
-};
+const getChallenge = asyncHandler(async (req, res) => {
+  const challenges = await Challenge.find();
+  res.status(200).json(challenges);
+});
 
 // @desc set challenge
 // @route POST /api/challenge
 // @access Private
 
-const setChallenge = (req, res) => {
-  res.status(200).json({message: 'set challenge'});
-};
+const setChallenge = asyncHandler(async (req, res) => {
+  console.log(req.body.text);
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error('please add a challenge');
+  }
+
+  const challenges = await Challenge.create({
+    text: req.body.text,
+  });
+  res.status(200).json(challenges);
+});
 
 // @desc update challenge
 // @route UPDATE /api/challenge/:id
 // @access Private
 
-const updateChallenge = (req, res) => {
-  res.status(200).json({message: `update challenge ${req.params.id}`});
-};
+const updateChallenge = asyncHandler(async (req, res) => {
+  const challenges = await Challenge.findById(req.params.id);
+  if (!challenges) {
+    res.status(400);
+    throw new Error('Challenge not found');
+  }
+
+  const updatedChallenge = await Challenge.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {
+      new: true,
+    },
+  );
+  res.status(200).json(updatedChallenge);
+});
 
 // @desc delete challenge
 // @route DELETE /api/challenge/:id
 // @access Private
 
-const deleteChallenge = (req, res) => {
-  res.status(200).json({message: `delete challenge ${req.params.id}`});
-};
+const deleteChallenge = asyncHandler(async (req, res) => {
+  const challenges = await Challenge.findById(req.params.id);
+  if (!challenges) {
+    res.status(400);
+    throw new Error('Challenge not found');
+  }
+
+  await challenges.remove();
+  res.status(200).json({id: req.params.id});
+});
 
 module.exports = {
   getChallenge,
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require('express');
+const colors = require('colors');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
+
+connectDB();
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
 app.use('/api/challenge', require('./routes/challengeRoutes'));
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));
