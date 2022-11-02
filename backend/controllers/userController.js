12b614f314ad24b2b05b6321e8205fcb5dbf0a3e
Beth-Anne Farmer <bethannef@protonmail.com>
2022-11-02 13:13:10 -0400

    backend connected to mongo can register user in database

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,26 +1,66 @@
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+
 // @desc register New user
 // @route POST /api/user
 // @access Public
 
-const registerUser = (req, res) => {
-  res.json({message: 'Register User'});
-};
+const registerUser = asyncHandler(async (req, res) => {
+  const {username, email, password} = req.body;
+
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error('Please add all fields!');
+  }
+
+  //check if user exists
+  const userExists = await User.findOne({email});
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists!');
+  }
+
+  //hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await User.create({
+    username,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user.id,
+      username: user.username,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid User Data');
+  }
+});
 
 // @desc authenticate New user
 // @route POST /api/user/login
 // @access Public
 
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
   res.json({message: 'Login User'});
-};
+});
 
 // @desc Get user data
 // @route GET /api/user/me
 // @access Public
 
-const getMe = (req, res) => {
+const getMe = asyncHandler(async (req, res) => {
   res.json({message: 'User data display'});
-};
+});
 
 module.exports = {
   registerUser,
